dd23e2c30998e143db9dd05e6dd3f705236acee6
mosesbalaji <moses.balaji-external@tcs.com>
2022-01-11 23:21:38 +0800

    List repo

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ for(const organization of organizationlist){
                     orgrepo.push(name);
                   })
               
-                  let orreposecret = [];
+                  let orreposecret = "";
                   let reposec = [];
                   let secretlist = [];
                   let repocont = [];
@@ -71,7 +71,7 @@ for(const organization of organizationlist){
                       reposec = await orgActivity1.getOrgRepoSecret(organization,orepo);
                       reposec.map(({name}) =>{
                       console.log(name,"repsec")
-                      orreposecret.push(name);
+                      orreposecret = name;
                       })
                       console.log(reposec,"repository sec")
                       console.log(secret,"secrets organization")
